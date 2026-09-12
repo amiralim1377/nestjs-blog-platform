d4681c828031e6c3437549fe32d55c748a9b990b
AMIRALI <alimoradi7718@gmail.com>
2026-09-12 17:10:34 +0330

    feat: enhance user creation logic with error handling and validation

--- src/modules/users/providers/users.service.ts
+++ src/modules/users/providers/users.service.ts
@@ -1,9 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  RequestTimeoutException,
+} from '@nestjs/common';
 import { CreateUserDto } from '../dto/create-user.dto.js';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../entities/user.entity.js';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    let existingUser = undefined;
+
+    try {
+      // Check if user with email exists
+      existingUser = await this.usersRepository.findOne({
+        where: { email: createUserDto.email },
+      });
+    } catch (error) {
+      throw new RequestTimeoutException(
+        'Unable to process your request at the moment please try later',
+        {
+          description: 'Error connecting to database',
+        },
+      );
+    }
+
+    if (existingUser) {
+      throw new BadRequestException(
+        'The user already exists, please check your email',
+      );
+    }
+
+    let newUser = this.usersRepository.create({
+      ...createUserDto,
+    });
+
+    try {
+      newUser = await this.usersRepository.save(newUser);
+    } catch (error) {
+      throw new RequestTimeoutException(
+        'Unable to process your request at the moment please try later',
+        {
+          description: 'Error connecting to database',
+        },
+      );
+    }
+
+    // Create the user
+    return newUser;
   }
 }
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service.js';
 import { UsersController } from './users.controller.js';
+import { UsersService } from './providers/users.service.js';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity.js';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService],
+  exports: [],
 })
 export class UsersModule {}
