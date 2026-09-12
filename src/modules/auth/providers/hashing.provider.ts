c301844b4e96643933337c47ec8978d8a1a98087
AMIRALI <alimoradi7718@gmail.com>
2026-09-12 19:36:13 +0330

    refactor: remove unused DTOs and hashing provider implementation

--- src/modules/auth/providers/hashing.provider.ts
+++
@@ -1,8 +0,0 @@
-export abstract class HashingProvider {
-  abstract hashPassword(data: string | Buffer): Promise<string>;
-
-  abstract comparePassword(
-    data: string | Buffer,
-    encrypted: string,
-  ): Promise<boolean>;
-}
