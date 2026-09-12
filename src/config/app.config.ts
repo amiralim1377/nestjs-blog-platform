9f53597b0245b822cf77101acb5f3aba467a42e7
AMIRALI <alimoradi7718@gmail.com>
2026-09-12 18:19:23 +0330

    feat: add API version configuration to app settings

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -3,4 +3,5 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('appConfig', () => ({
   environment: process.env.NODE_ENV || 'production',
   port: parseInt(process.env.PORT || '3000', 10),
+  apiVersion: process.env.API_VERSION,
 }));
