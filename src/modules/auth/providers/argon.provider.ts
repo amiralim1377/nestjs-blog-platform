9b5f58a878d2fc85a59701835c3161a69b4b7574
AMIRALI <alimoradi7718@gmail.com>
2026-09-12 18:19:41 +0330

    feat: implement ArgonProvider for password hashing and comparison

---
+++ src/modules/auth/providers/argon.provider.ts
@@ -0,0 +1,15 @@
+import { HashingProvider } from './hashing.provider.js';
+import * as argon2 from 'argon2';
+
+export class ArgonProvider implements HashingProvider {
+  async hashPassword(data: string | Buffer): Promise<string> {
+    return await argon2.hash(data);
+  }
+
+  async comparePassword(
+    data: string | Buffer,
+    encrypted: string,
+  ): Promise<boolean> {
+    return await argon2.verify(encrypted, data);
+  }
+}
