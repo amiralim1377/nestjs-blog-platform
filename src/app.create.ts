e798db6cc05904f4b0af9b1bd54f676cc8c0a738
AMIRALI <alimoradi7718@gmail.com>
2026-09-12 16:13:19 +0330

    Refactor code structure for improved readability and maintainability

--- src/app.create.ts
+++ src/app.create.ts
@@ -1,5 +1,6 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export function appCreate(app: INestApplication): void {
   // use validation pipes
@@ -16,6 +17,20 @@ export function appCreate(app: INestApplication): void {
 
   //   pino-loger-config
   app.useLogger(app.get(Logger));
+
+  //   swagger-config
+  const config = new DocumentBuilder()
+    .setTitle('Blog app Api')
+    .setDescription('use the base API URL as http://localhost:3000')
+    .setTermsOfService('http://localhost:3000/terms-of-service')
+    .setLicense('MIT License', 'https://opensource.org/license/mit/')
+    .addServer('http://localhost:3000')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
 
   app.enableCors();
 }
