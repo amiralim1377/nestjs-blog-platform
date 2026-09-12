bdf523519539bb405e142ac2760002829ab43724
AMIRALI <alimoradi7718@gmail.com>
2026-09-12 18:19:34 +0330

    feat: configure HashingProvider to use ArgonProvider for password hashing

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './providers/auth.service.js';
 import { AuthController } from './auth.controller.js';
+import { HashingProvider } from './providers/hashing.provider.js';
+import { ArgonProvider } from './providers/argon.provider.js';
 
 @Module({
   imports: [],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    {
+      provide: HashingProvider,
+      useClass: ArgonProvider,
+    },
+  ],
+  exports: [HashingProvider],
 })
 export class AuthModule {}
