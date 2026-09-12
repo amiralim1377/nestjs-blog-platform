738c2b37ebb75f1d68a7c07ed6597d5ddece24a4
AMIRALI <alimoradi7718@gmail.com>
2026-09-12 15:49:35 +0330

    feat: configure global settings with ConfigModule and environment validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from './config/app.config.js';
+import enviromentValidation from './config/enviroment.validation.js';
 
+const ENV = process.env.NODE_ENV;
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: !ENV ? '.env' : `.env.${ENV}`,
+      load: [appConfig],
+      validationSchema: enviromentValidation,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
