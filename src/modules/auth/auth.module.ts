1a41fdbe216af3e5329f2e149b2f970e06842c89
AMIRALI <alimoradi7718@gmail.com>
2026-09-12 19:37:41 +0330

    fix: correct import path for ArgonProvider in AuthModule

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,11 +2,11 @@ import { forwardRef, Module } from '@nestjs/common';
 import { AuthService } from './providers/auth.service.js';
 import { AuthController } from './auth.controller.js';
 import { HashingProvider } from './providers/hashing/hashing.provider.js';
-import { ArgonProvider } from './providers/argon.provider.js';
 import { UsersModule } from '../users/users.module.js';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule } from '@nestjs/config';
 import jwtConfig from './config/jwt.config.js';
+import { ArgonProvider } from './providers/hashing/argon.provider.js';
 
 @Module({
   imports: [
