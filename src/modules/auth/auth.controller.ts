684ec4b84719da2ea8c72d5d753d419daf1a707a
AMIRALI <alimoradi7718@gmail.com>
2026-09-12 21:02:42 +0330

    feat: add ActiveUser decorator for extracting user data from request

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './providers/auth.service.js';
 import { LoginDto } from './dto/login.dto.js';
 import { RefreshTokenDto } from './dto/refresh-token.dto.js';
+import { Auth } from './decorator/auth.decorator.js';
+import { AuthType } from './enums/auth-type.enum.js';
 
 @Controller('auth')
 export class AuthController {
@@ -9,12 +11,14 @@ export class AuthController {
 
   @Post('login')
   @HttpCode(HttpStatus.OK)
+  @Auth(AuthType.None)
   async login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
   }
 
   @Post('refresh-tokens')
   @HttpCode(HttpStatus.CREATED)
+  @Auth(AuthType.None)
   async createRefreshTokens(@Body() refreshTokenDto: RefreshTokenDto) {
     return this.authService.refreshTokens(refreshTokenDto);
   }
