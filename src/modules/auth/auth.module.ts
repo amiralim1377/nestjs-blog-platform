911ee3c74a9b38132b9617897006c38d1ac56b41
AMIRALI <alimoradi7718@gmail.com>
2026-09-12 18:28:03 +0330

    feat: enhance AuthModule with user and JWT configuration imports

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,11 +1,19 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AuthService } from './providers/auth.service.js';
 import { AuthController } from './auth.controller.js';
 import { HashingProvider } from './providers/hashing.provider.js';
 import { ArgonProvider } from './providers/argon.provider.js';
+import { UsersModule } from '../users/users.module.js';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from './config/jwt.config.js';
 
 @Module({
-  imports: [],
+  imports: [
+    forwardRef(() => UsersModule),
+    ConfigModule.forFeature(jwtConfig),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+  ],
   controllers: [AuthController],
   providers: [
     AuthService,
