e5b6e87e2213cab82c891bb3fe5ed2ef7af99e5f
AMIRALI <alimoradi7718@gmail.com>
2026-09-12 15:53:18 +0330

    feat: add database configuration for environment settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service.js';
 import { ConfigModule } from '@nestjs/config';
 import appConfig from './config/app.config.js';
 import enviromentValidation from './config/enviroment.validation.js';
+import databaseConfig from './config/database.config.js';
 
 const ENV = process.env.NODE_ENV;
 @Module({
@@ -11,7 +12,7 @@ const ENV = process.env.NODE_ENV;
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: !ENV ? '.env' : `.env.${ENV}`,
-      load: [appConfig],
+      load: [appConfig, databaseConfig],
       validationSchema: enviromentValidation,
     }),
   ],
