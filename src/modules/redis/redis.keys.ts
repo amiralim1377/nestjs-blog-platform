26a4b39cffbe946020e139fca3ec89413fc590a8
AMIRALI <alimoradi7718@gmail.com>
2026-09-13 15:57:46 +0330

    feat: add getAccountLockoutKey function for account lockout management

--- src/modules/redis/redis.keys.ts
+++ src/modules/redis/redis.keys.ts
@@ -4,4 +4,5 @@ export const RedisKeys = {
    * @example 'blacklist:9a59670a-cbf0-420e-a7ad-deba3fcb1589'
    */
   blacklistToken: (jti: string) => `blacklist:${jti}`,
+  getAccountLockoutKey: (email: string) => `account_lockout:${email}`,
 };
