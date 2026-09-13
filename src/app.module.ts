9293a8b5dd9e0f4d56303e19fb41eeb54333ecda
AMIRALI <alimoradi7718@gmail.com>
2026-09-13 20:05:12 +0330

    fix: update ThrottlerModule imports to be an array

--- src/app.module.ts
+++ src/app.module.ts
@@ -68,7 +68,7 @@ const ENV = process.env.NODE_ENV;
       },
     }),
     ThrottlerModule.forRootAsync({
-      imports: ConfigModule,
+      imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         throttlers: [
