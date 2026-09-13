c6e4230deab1cc399b2339dffb1e2f9cdd5646c2
AMIRALI <alimoradi7718@gmail.com>
2026-09-13 18:07:57 +0330

    feat: add CreatePostDto for blog post creation with validation rules

---
+++ src/modules/posts/dto/create-post.dto.ts
@@ -0,0 +1,54 @@
+import {
+  IsEnum,
+  IsISO8601,
+  IsJSON,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+  Matches,
+  MaxLength,
+} from 'class-validator';
+import { PostType } from '../enums/postType.enum.js';
+import { PostStatus } from '../enums/postStatus.enum.js';
+
+export class CreatePostDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(512)
+  title: string;
+
+  @IsEnum(PostType)
+  @IsNotEmpty()
+  postType: PostType;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(256)
+  @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
+    message:
+      'اسلاگ (Slug) باید فقط شامل حروف کوچک انگلیسی، اعداد و خط تیره باشد (بدون فاصله). مثال: my-first-post',
+  })
+  slug: string;
+
+  @IsEnum(PostStatus)
+  @IsNotEmpty()
+  status: PostStatus;
+
+  @IsString()
+  @IsOptional()
+  content?: string;
+
+  @IsJSON()
+  @IsOptional()
+  schema?: string;
+
+  @IsUrl()
+  @MaxLength(1024)
+  @IsOptional()
+  featuredImageUrl?: string;
+
+  @IsISO8601()
+  @IsOptional()
+  publishOn?: Date;
+}
