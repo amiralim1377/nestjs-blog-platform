4eba3f9b73ff27333dda806a5ade21a0796e8d0b
AMIRALI <alimoradi7718@gmail.com>
2026-09-13 13:48:10 +0330

    feat: add Auth decorator to UsersController for authentication handling

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -7,12 +7,15 @@ import {
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto.js';
 import { UsersService } from './providers/users.service.js';
+import { AuthType } from '../auth/enums/auth-type.enum.js';
+import { Auth } from '../auth/decorator/auth.decorator.js';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @Auth(AuthType.None)
   @UseInterceptors(ClassSerializerInterceptor)
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
