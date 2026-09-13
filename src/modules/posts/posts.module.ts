5f5bb9a6c29e80dec3f0cb14c6773217c56b50f0
AMIRALI <alimoradi7718@gmail.com>
2026-09-13 20:05:30 +0330

    feat: update PostsModule imports to include TypeOrmModule and PaginationModule

--- src/modules/posts/posts.module.ts
+++ src/modules/posts/posts.module.ts
@@ -1,19 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { PostsService } from './providers/posts.service.js';
 import { PostsController } from './posts.controller.js';
+
 import { UsersModule } from '../users/users.module.js';
+
+import { Post } from './entities/post.entity.js';
+
 import { CreatePostProvider } from './providers/actions/create-post.provider.js';
 import { UpdatePostProvider } from './providers/actions/update-post.provider.js';
 import { DeletePostProvider } from './providers/actions/delete-post.provider.js';
+import { PaginationModule } from '../../common/pagination/pagination.module.js';
+import { FindAllPostsProvider } from './providers/actions/find-all-post.provider.js';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, TypeOrmModule.forFeature([Post]), PaginationModule],
   controllers: [PostsController],
   providers: [
     PostsService,
     CreatePostProvider,
     UpdatePostProvider,
     DeletePostProvider,
+    FindAllPostsProvider,
   ],
   exports: [],
 })
