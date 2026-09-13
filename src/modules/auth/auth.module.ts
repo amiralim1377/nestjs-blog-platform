91139214efe44a68fa74e5989426cb406ccca8ef
AMIRALI <alimoradi7718@gmail.com>
2026-09-13 14:27:05 +0330

    feat: fix import path for LoginProvider and add RegisterProvider to AuthModule

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -8,13 +8,14 @@ import { ConfigModule } from '@nestjs/config';
 import jwtConfig from './config/jwt.config.js';
 import { ArgonProvider } from './providers/hashing/argon.provider.js';
 import { RefreshTokensProvider } from './providers/authentication/refresh-tokens.provider.js';
-import { LoginProvider } from './providers/authentication/login.provider.js';
+import { LoginProvider } from './providers/authentication/login-provider.js';
 import { GenerateTokensProvider } from './providers/tokens/generate-tokens.provider.js';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthenticationGuard } from './guards/authentication/authentication.guard.js';
 import { AccessTokenGuard } from './guards/access-token/access-token.guard.js';
 import { LogoutProvider } from './providers/authentication/logout-provider.js';
 import { CookieProvider } from './providers/cookie/cookie.provider.js';
+import { RegisterProvider } from './providers/authentication/register-provider.js';
 
 @Module({
   imports: [
@@ -27,6 +28,7 @@ import { CookieProvider } from './providers/cookie/cookie.provider.js';
     AuthService,
     LoginProvider,
     LogoutProvider,
+    RegisterProvider,
     RefreshTokensProvider,
     GenerateTokensProvider,
     AccessTokenGuard,
