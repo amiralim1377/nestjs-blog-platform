1787f36e4aeaef7100c4d36558a15279afb0c089
AMIRALI <alimoradi7718@gmail.com>
2026-09-13 11:24:52 +0330

    feat: add logout method to AuthService for handling user logout

--- src/modules/auth/providers/auth.service.ts
+++ src/modules/auth/providers/auth.service.ts
@@ -3,12 +3,14 @@ import { LoginDto } from '../dto/login.dto.js';
 import { LoginProvider } from './authentication/login.provider.js';
 import { RefreshTokenDto } from '../dto/refresh-token.dto.js';
 import { RefreshTokensProvider } from './authentication/refresh-tokens.provider.js';
+import { LogoutProvider } from './authentication/logout-provider.js';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly loginProvider: LoginProvider,
     private readonly refreshTokenProvider: RefreshTokensProvider,
+    private readonly logoutProvider: LogoutProvider,
   ) {}
 
   async login(loginDto: LoginDto) {
@@ -17,5 +19,9 @@ export class AuthService {
 
   public async refreshTokens(refreshTokenDto: RefreshTokenDto) {
     return await this.refreshTokenProvider.refreshTokens(refreshTokenDto);
+  }
+
+  async logout(accessToken: string) {
+    return await this.logoutProvider.logout(accessToken);
   }
 }
