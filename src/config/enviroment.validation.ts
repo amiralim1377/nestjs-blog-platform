0fd10aab45d356ea3d2f2789e550661bd4767100
AMIRALI <alimoradi7718@gmail.com>
2026-09-13 12:45:29 +0330

    feat: add JWT_REFRESH_TOKEN_SECRET to environment validation

--- src/config/enviroment.validation.ts
+++ src/config/enviroment.validation.ts
@@ -10,6 +10,7 @@ export default Joi.object({
   DATABASE_NAME: Joi.string().required(),
   DATABASE_USER: Joi.string().required(),
   JWT_SECRET: Joi.string().required(),
+  JWT_REFRESH_TOKEN_SECRET: Joi.string().required(),
   JWT_TOKEN_AUDIENCE: Joi.required(),
   JWT_TOKEN_ISSUER: Joi.string().required(),
   JWT_ACCESS_TOKEN_TTL: Joi.number().required(),
