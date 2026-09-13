ba46aa1c06368623918de468e58e022ae531e0bc
AMIRALI <alimoradi7718@gmail.com>
2026-09-13 12:46:16 +0330

    feat: enforce non-null assertion for JWT_SECRET and add refreshTokenSecret

--- src/modules/auth/config/jwt.config.ts
+++ src/modules/auth/config/jwt.config.ts
@@ -2,7 +2,8 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('jwt', () => {
   return {
-    secret: process.env.JWT_SECRET,
+    secret: process.env.JWT_SECRET!,
+    refreshTokenSecret: process.env.JWT_REFRESH_TOKEN_SECRET!,
     audience: process.env.JWT_TOKEN_AUDIENCE,
     issuer: process.env.JWT_TOKEN_ISSUER,
     accessTokenTtl: parseInt(process.env.JWT_ACCESS_TOKEN_TTL ?? '3600', 10),
