644ef6263f91c741a6b77cb744f848dab40df6a7
AMIRALI <alimoradi7718@gmail.com>
2026-09-14 12:48:49 +0330

    feat: refactor create method in UsersService to utilize CreateUserProvider for user creation

--- src/modules/users/providers/users.service.ts
+++ src/modules/users/providers/users.service.ts
@@ -7,56 +7,17 @@ import { CreateUserDto } from '../dto/create-user.dto.js';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../entities/user.entity.js';
 import { Repository } from 'typeorm';
-import { HashingProvider } from '../../auth/providers/hashing/hashing.provider.js';
+import { CreateUserProvider } from './actions/create-user.provider.js';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private usersRepository: Repository<User>,
-    private readonly hashingProvider: HashingProvider,
+    private readonly createUserProvider: CreateUserProvider,
   ) {}
 
   async create(createUserDto: CreateUserDto) {
-    let existingUser = undefined;
-
-    try {
-      // Check if user with email exists
-      existingUser = await this.usersRepository.findOne({
-        where: { email: createUserDto.email },
-      });
-    } catch {
-      throw new RequestTimeoutException(
-        'Unable to process your request at the moment please try later',
-        {
-          description: 'Error connecting to database',
-        },
-      );
-    }
-
-    if (existingUser) {
-      throw new BadRequestException(
-        'The user already exists, please check your email',
-      );
-    }
-
-    let newUser = this.usersRepository.create({
-      ...createUserDto,
-      password: await this.hashingProvider.hashPassword(createUserDto.password),
-    });
-
-    try {
-      newUser = await this.usersRepository.save(newUser);
-    } catch {
-      throw new RequestTimeoutException(
-        'Unable to process your request at the moment please try later',
-        {
-          description: 'Error connecting to database',
-        },
-      );
-    }
-
-    // Create the user
-    return newUser;
+    return this.createUserProvider.create(createUserDto);
   }
 
   async findByEmail(email: string) {
