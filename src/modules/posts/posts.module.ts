2d2a6f3d36c06e9d9cfd87c40d712fb2110b5a22
AMIRALI <alimoradi7718@gmail.com>
2026-09-14 19:14:42 +0330

    feat: add FindPostByIdProvider to PostsModule for retrieving posts by ID

--- src/modules/posts/posts.module.ts
+++ src/modules/posts/posts.module.ts
@@ -1,19 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
 import { PostsService } from './providers/posts.service.js';
 import { PostsController } from './posts.controller.js';
-
 import { UsersModule } from '../users/users.module.js';
-
 import { Post } from './entities/post.entity.js';
-
 import { CreatePostProvider } from './providers/actions/create-post.provider.js';
 import { UpdatePostProvider } from './providers/actions/update-post.provider.js';
 import { DeletePostProvider } from './providers/actions/delete-post.provider.js';
 import { PaginationModule } from '../../common/pagination/pagination.module.js';
 import { FindAllPostsProvider } from './providers/actions/find-all-post.provider.js';
 import { FindPostBySlugProvider } from './providers/actions/find-post-by-slug.js';
+import { FindPostByIdProvider } from './providers/actions/find-by-id.provider.js';
 
 @Module({
   imports: [UsersModule, TypeOrmModule.forFeature([Post]), PaginationModule],
@@ -25,6 +22,7 @@ import { FindPostBySlugProvider } from './providers/actions/find-post-by-slug.js
     DeletePostProvider,
     FindAllPostsProvider,
     FindPostBySlugProvider,
+    FindPostByIdProvider,
   ],
   exports: [],
 })
