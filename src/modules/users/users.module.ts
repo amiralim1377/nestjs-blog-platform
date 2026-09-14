224861e8eb4a18246765e040ad1c6a13778c2fdc
AMIRALI <alimoradi7718@gmail.com>
2026-09-14 12:48:32 +0330

    feat: add CreateUserProvider to UsersModule for user creation functionality

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -4,11 +4,12 @@ import { UsersService } from './providers/users.service.js';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity.js';
 import { AuthModule } from '../auth/auth.module.js';
+import { CreateUserProvider } from './providers/actions/create-user.provider.js';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), forwardRef(() => AuthModule)],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, CreateUserProvider],
   exports: [UsersService],
 })
 export class UsersModule {}
