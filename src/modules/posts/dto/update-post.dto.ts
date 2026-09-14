3b89b20534116082c49beca78e4c945221328142
AMIRALI <alimoradi7718@gmail.com>
2026-09-14 16:29:33 +0330

    refactor: simplify UpdatePostDto by removing unnecessary properties and decorators

--- src/modules/posts/dto/update-post.dto.ts
+++ src/modules/posts/dto/update-post.dto.ts
@@ -1,10 +1,5 @@
-import { ApiProperty, PartialType } from '@nestjs/swagger';
-import { IsInt, IsNotEmpty } from 'class-validator';
+import { PartialType } from '@nestjs/swagger';
+
 import { CreatePostDto } from './create-post.dto.js';
 
-export class UpdatePostDto extends PartialType(CreatePostDto) {
-  @ApiProperty()
-  @IsInt()
-  @IsNotEmpty()
-  id: number;
-}
+export class UpdatePostDto extends PartialType(CreatePostDto) {}
