60e772f0db3321f8a4143f54581d0aee7fcb29a5
AMIRALI <alimoradi7718@gmail.com>
2026-09-14 17:41:03 +0330

    fix: remove unused UsersService and clean up findAll method in FindAllPostsProvider

--- src/modules/posts/providers/actions/find-all-post.provider.ts
+++ src/modules/posts/providers/actions/find-all-post.provider.ts
@@ -1,17 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { UsersService } from '../../../users/providers/users.service.js';
 import { Post } from '../../entities/post.entity.js';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
-import { ActiveUserData } from '../../../auth/interfaces/active-user-data.interface.js';
-import { UpdatePostDto } from '../../dto/update-post.dto.js';
 import { GetPostsDto } from '../../dto/get-posts.dto.js';
 import { PaginationProvider } from '../../../../common/pagination/providers/pagination.providers.js';
 
 @Injectable()
 export class FindAllPostsProvider {
   constructor(
-    private usersService: UsersService,
     @InjectRepository(Post)
     private postRepository: Repository<Post>,
     private readonly paginationProvider: PaginationProvider,
@@ -29,12 +25,8 @@ export class FindAllPostsProvider {
       currentUrl,
       {
         where: whereConditions,
-        relations: {
-          author: true,
-        },
-        order: {
-          publishOn: 'DESC',
-        },
+        order: { publishOn: 'DESC' },
+        relations: { author: true },
       },
     );
   }
