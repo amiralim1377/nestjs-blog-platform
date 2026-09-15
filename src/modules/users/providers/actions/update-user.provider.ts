93c9432708395c96f2cbcd4347bd114bb0cc6362
AMIRALI <alimoradi7718@gmail.com>
2026-09-15 10:26:54 +0330

    feat: implement UpdateUserProvider for user information updates

---
+++ src/modules/users/providers/actions/update-user.provider.ts
@@ -0,0 +1,33 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { UpdateUserDto } from '../../dto/update-user.dto.js';
+import { User } from '../../entities/user.entity.js';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class UpdateUserProvider {
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+
+  async updateUserInfo(
+    userId: string,
+    updateUserDto: UpdateUserDto,
+  ): Promise<User> {
+    if (updateUserDto.password) {
+      throw new BadRequestException('This route is not for updating password');
+    }
+
+    const user = await this.usersRepository.preload({
+      id: userId,
+      ...updateUserDto,
+    });
+
+    if (!user) {
+      throw new BadRequestException('User not found');
+    }
+
+    return this.usersRepository.save(user);
+  }
+}
