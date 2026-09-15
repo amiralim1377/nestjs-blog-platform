14fd6c4b25275420d8529d92df66c50218552276
AMIRALI <alimoradi7718@gmail.com>
2026-09-15 18:43:25 +0330

    refactor: update sendMail method to accept SendResetPasswordDto for better structure

--- src/modules/mail/providers/actions/send-reset-password-mail.provider.ts
+++ src/modules/mail/providers/actions/send-reset-password-mail.provider.ts
@@ -4,6 +4,8 @@ import {
   Logger,
 } from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer';
+import { ResetPasswordTemplateContext } from '../../interfaces/mail-template.interface.js';
+import { SendResetPasswordDto } from '../../dto/send-reset-password.dto.js';
 
 @Injectable()
 export class SendResetPasswordMailProvider {
@@ -11,21 +13,21 @@ export class SendResetPasswordMailProvider {
   constructor(private readonly mailerService: MailerService) {}
 
   public async sendMail(
-    email: string,
-    resetLink: string,
-    userName: string,
+    sendResetPasswordDto: SendResetPasswordDto,
   ): Promise<boolean> {
+    const { to, name, resetLink } = sendResetPasswordDto;
+
     try {
       await this.mailerService.sendMail({
-        to: email,
+        to: to,
         subject: 'Reset Your Password - Action Required',
         template: './reset-password',
         context: {
-          name: userName,
+          name: name,
           resetLink: resetLink,
-        },
+        } as ResetPasswordTemplateContext,
       });
-      this.logger.log(`Reset password email successfully sent to ${email}`);
+      this.logger.log(`Reset password email successfully sent to ${to}`);
       return true;
     } catch (error) {
       throw new InternalServerErrorException('Error sending email');
