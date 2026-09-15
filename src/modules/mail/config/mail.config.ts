f48532bdc6087d7095d990c851cf5a6ccb31be71
AMIRALI <alimoradi7718@gmail.com>
2026-09-15 17:57:29 +0330

    fix: correct MAIL_PORT fallback to ensure proper default value

--- src/modules/mail/config/mail.config.ts
+++ src/modules/mail/config/mail.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('mail', () => ({
   host: process.env.MAIL_HOST,
-  port: parseInt(process.env.MAIL_PORT, 10) || 587,
+  port: parseInt(process.env.MAIL_PORT || '587', 10),
   ignoreTLS: process.env.MAIL_IGNORE_TLS === 'true',
   secure: process.env.MAIL_SECURE === 'true',
   user: process.env.MAIL_USER,
