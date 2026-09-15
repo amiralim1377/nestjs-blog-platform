09b1d303e4d82d905d65ad1a6d299b233529479e
AMIRALI <alimoradi7718@gmail.com>
2026-09-15 10:24:41 +0330

    feat: add update method to UsersController for updating user information

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -4,11 +4,17 @@ import {
   Body,
   UseInterceptors,
   ClassSerializerInterceptor,
+  Patch,
+  Param,
+  HttpCode,
+  HttpStatus,
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto.js';
 import { UsersService } from './providers/users.service.js';
 import { AuthType } from '../auth/enums/auth-type.enum.js';
 import { Auth } from '../auth/decorator/auth.decorator.js';
+import { UpdateUserDto } from './dto/update-user.dto.js';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
@@ -19,5 +25,29 @@ export class UsersController {
   @UseInterceptors(ClassSerializerInterceptor)
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
+  }
+
+  @Patch(':userId')
+  @HttpCode(HttpStatus.OK)
+  @Auth(AuthType.Bearer, AuthType.Cookie)
+  @UseInterceptors(ClassSerializerInterceptor)
+  @ApiOperation({ summary: 'Updates the authenticated user information.' })
+  @ApiResponse({
+    status: 200,
+    description: 'User information updated successfully.',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Invalid user information.',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'User not found.',
+  })
+  async update(
+    @Param('userId') userId: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return this.usersService.update(userId, updateUserDto);
   }
 }
