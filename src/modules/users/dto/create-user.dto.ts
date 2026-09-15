e6794e2df1cbb801db7e63499c3b3e91d2db2e56
AMIRALI <alimoradi7718@gmail.com>
2026-09-15 11:12:35 +0330

    fix: update password regex in CreateUserDto to allow hyphens

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -28,7 +28,7 @@ export class CreateUserDto {
   @IsString()
   @IsNotEmpty()
   @MinLength(8)
-  @Matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, {
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&-]{8,}$/, {
     message:
       'Minimum eight characters, at least one letter, one number and one special character',
   })
