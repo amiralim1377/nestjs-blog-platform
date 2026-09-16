a39513c1a47bd1b7030c85a2919875a6259cb4af
AMIRALI <alimoradi7718@gmail.com>
2026-09-16 16:06:56 +0330

    refactor: update sendMail method to accept UserCreatedEvent for better clarity and type safety

--- src/modules/mail/providers/actions/send-welcome-mail.provider.ts
+++ src/modules/mail/providers/actions/send-welcome-mail.provider.ts
@@ -14,21 +14,24 @@ export class SendWelcomeMailProvider {
   constructor(private readonly mailerService: MailerService) {}
 
   @OnEvent(UserCreatedEvent.name, { async: true })
-  public async sendMail(email: string, userName: string): Promise<boolean> {
+  public async sendMail(event: UserCreatedEvent): Promise<boolean> {
     try {
       await this.mailerService.sendMail({
-        to: email,
+        to: event.email,
         subject: 'Welcome to Our Application! 🎉',
         template: './welcome',
         context: {
-          name: userName,
+          name: event.firstName,
         },
       });
-      this.logger.log(`Welcome email successfully sent to ${email}`);
+      this.logger.log(`Welcome email successfully sent to ${event.email}`);
       return true;
     } catch (error) {
       const stack = error instanceof Error ? error.stack : String(error);
-      this.logger.error(`Failed to send welcome email to ${email}`, stack);
+      this.logger.error(
+        `Failed to send welcome email to ${event.email}`,
+        stack,
+      );
       throw new InternalServerErrorException('Error sending welcome email');
     }
   }
