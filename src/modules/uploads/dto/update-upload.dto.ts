c2550f626abdee8defd7b67c4ee66db4c1e720d3
AMIRALI <alimoradi7718@gmail.com>
2026-09-16 12:10:27 +0330

    refactor: simplify UpdateUploadDto by removing OmitType for file property

--- src/modules/uploads/dto/update-upload.dto.ts
+++ src/modules/uploads/dto/update-upload.dto.ts
@@ -1,6 +1,4 @@
-import { OmitType, PartialType } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/swagger';
 import { CreateUploadDto } from './create-upload.dto.js';
 
-export class UpdateUploadDto extends PartialType(
-  OmitType(CreateUploadDto, ['file'] as const),
-) {}
+export class UpdateUploadDto extends PartialType(CreateUploadDto) {}
