9445eac7959d838f0093749684f0265d62953711
AMIRALI <alimoradi7718@gmail.com>
2026-09-16 16:55:42 +0330

    fix: remove unused sendResetPasswordMail method and related dependencies from MailService

--- src/modules/mail/providers/mail.service.ts
+++ src/modules/mail/providers/mail.service.ts
@@ -1,16 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { SendResetPasswordMailProvider } from './actions/send-reset-password-mail.provider.js';
-import { SendResetPasswordDto } from '../dto/send-reset-password.dto.js';
 
 @Injectable()
 export class MailService {
-  constructor(
-    private readonly sendResetPasswordMailProvider: SendResetPasswordMailProvider,
-  ) {}
-
-  public async sendResetPasswordMail(
-    sendResetPasswordDto: SendResetPasswordDto,
-  ) {
-    this.sendResetPasswordMailProvider.sendMail(sendResetPasswordDto);
-  }
+  constructor() {}
 }
