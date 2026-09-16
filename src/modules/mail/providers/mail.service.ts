4b475a136a83431bc0d1e296ec4be6a4863173c9
AMIRALI <alimoradi7718@gmail.com>
2026-09-16 16:30:17 +0330

    fix: remove unused sendWelcomeMail method and provider from MailService

--- src/modules/mail/providers/mail.service.ts
+++ src/modules/mail/providers/mail.service.ts
@@ -1,22 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { SendResetPasswordMailProvider } from './actions/send-reset-password-mail.provider.js';
-import { SendWelcomeMailProvider } from './actions/send-welcome-mail.provider.js';
 import { SendResetPasswordDto } from '../dto/send-reset-password.dto.js';
 
 @Injectable()
 export class MailService {
   constructor(
     private readonly sendResetPasswordMailProvider: SendResetPasswordMailProvider,
-    private readonly sendWelcomeMailProvider: SendWelcomeMailProvider,
   ) {}
 
   public async sendResetPasswordMail(
     sendResetPasswordDto: SendResetPasswordDto,
   ) {
     this.sendResetPasswordMailProvider.sendMail(sendResetPasswordDto);
-  }
-
-  public async sendWelcomeMail(email: string, userName: string) {
-    this.sendWelcomeMailProvider.sendMail(email, userName).catch(() => {});
   }
 }
