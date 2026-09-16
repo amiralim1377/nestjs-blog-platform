9edaf4b28e5b872634886d9cfb106b492e4e8597
AMIRALI <alimoradi7718@gmail.com>
2026-09-16 20:07:58 +0330

    feat: define Tag entity with properties and relationships

--- src/tags/entities/tag.entity.ts
+++ src/tags/entities/tag.entity.ts
@@ -1,1 +1,33 @@
-export class Tag {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToMany,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Post } from '../../modules/posts/entities/post.entity.js';
+
+@Entity('tags')
+export class Tag {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 128, unique: true })
+  name: string;
+
+  @Column({ type: 'varchar', length: 128, unique: true })
+  slug: string;
+
+  @Column({ type: 'text', nullable: true })
+  description?: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @ManyToMany(() => Post, (post) => post.tags)
+  posts: Post[];
+}
