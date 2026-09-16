c68daa5eff72bc918725c0389fea38754f1c3323
AMIRALI <alimoradi7718@gmail.com>
2026-09-16 11:24:33 +0330

    feat: update uploadFile method in StorageService to return FileMetadata

--- src/modules/uploads/interfaces/storage-service.interface.ts
+++ src/modules/uploads/interfaces/storage-service.interface.ts
@@ -1,7 +1,6 @@
+import { FileMetadata } from './file-metadata.interface.js';
+
 export interface StorageService {
-  uploadFile(
-    file: Express.Multer.File,
-    folder?: string,
-  ): Promise<{ path: string; url: string }>;
+  uploadFile(file: Express.Multer.File, folder?: string): Promise<FileMetadata>;
   deleteFile(path: string): Promise<boolean>;
 }
