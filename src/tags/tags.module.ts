3c08385040197d52eb1b3c37baa5cc2231e08627
AMIRALI <alimoradi7718@gmail.com>
2026-09-16 20:06:33 +0330

    fix: update import paths and add providers in TagsModule for better structure

--- src/tags/tags.module.ts
+++ src/tags/tags.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { TagsService } from './tags.service.js';
+import { TagsService } from './providers/tags.service.js';
 import { TagsController } from './tags.controller.js';
+import { CreateTagProvider } from './providers/actions/create-tag.provider.js';
+import { UpdateTagProvider } from './providers/actions/update-tag.provider.js';
 
 @Module({
   controllers: [TagsController],
-  providers: [TagsService],
+  providers: [TagsService, CreateTagProvider, UpdateTagProvider],
 })
 export class TagsModule {}
