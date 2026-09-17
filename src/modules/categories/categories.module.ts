ab08a02428bc7f050c053fb98e8e0ae4421a35fe
AMIRALI <alimoradi7718@gmail.com>
2026-09-17 14:48:16 +0330

    feat: restructure Categories module to include providers for category actions

--- src/modules/categories/categories.module.ts
+++ src/modules/categories/categories.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
-import { CategoriesService } from './categories.service.js';
 import { CategoriesController } from './categories.controller.js';
+import { CategoriesService } from './providers/categories.service.js';
+import { AutocompleteCategoryProvider } from './providers/actions/autocomplete-category.provider.js';
+import { CreateCategoryProvider } from './providers/actions/create-category.provider.js';
+import { UpdateCategoryProvider } from './providers/actions/update-category.provider.js';
+import { DeleteCategoryProvider } from './providers/actions/delete-category.provider.js';
+import { PaginationModule } from '../../common/pagination/pagination.module.js';
+import { Category } from './entities/category.entity.js';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Category]), PaginationModule],
   controllers: [CategoriesController],
-  providers: [CategoriesService],
+  providers: [
+    CategoriesService,
+    CreateCategoryProvider,
+    AutocompleteCategoryProvider,
+    UpdateCategoryProvider,
+    DeleteCategoryProvider,
+  ],
 })
 export class CategoriesModule {}
