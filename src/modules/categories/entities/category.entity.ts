460e64d893f973daf213f4036ea182e5493310b8
AMIRALI <alimoradi7718@gmail.com>
2026-09-17 14:50:40 +0330

    feat: implement Category entity with properties and relationships

--- src/modules/categories/entities/category.entity.ts
+++ src/modules/categories/entities/category.entity.ts
@@ -1,1 +1,46 @@
-export class Category {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToMany,
+  ManyToOne,
+  OneToMany,
+  JoinColumn,
+} from 'typeorm';
+import { Post } from '../../posts/entities/post.entity.js';
+
+@Entity('categories')
+export class Category {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 128, unique: true })
+  name: string;
+
+  @Column({ type: 'varchar', length: 128, unique: true })
+  slug: string;
+
+  @Column({ type: 'text', nullable: true })
+  description?: string;
+
+  @ManyToOne(() => Category, (category) => category.children, {
+    onDelete: 'SET NULL',
+    nullable: true,
+  })
+  @JoinColumn({ name: 'parentId' })
+  parent?: Category;
+
+  @OneToMany(() => Category, (category) => category.parent)
+  children: Category[];
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @ManyToMany(() => Post, (post) => post.categories)
+  posts: Post[];
+}
