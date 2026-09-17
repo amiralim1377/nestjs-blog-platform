c75418f31ff3cc03537d4909b0b56c531eead9a1
AMIRALI <alimoradi7718@gmail.com>
2026-09-17 14:51:13 +0330

    feat: implement CategoriesService with CRUD operations and autocomplete suggestions

---
+++ src/modules/categories/providers/categories.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCategoryProvider } from './actions/create-category.provider.js';
+import { AutocompleteCategoryProvider } from './actions/autocomplete-category.provider.js';
+import { UpdateCategoryProvider } from './actions/update-category.provider.js';
+import { DeleteCategoryProvider } from './actions/delete-category.provider.js';
+import { FindAllCategoriesProvider } from './actions/find-all-categories.provider.js';
+import { GetCategoriesDto } from '../dto/get-categories.dto.js';
+import { UpdateCategoryDto } from '../dto/update-category.dto.js';
+import { CreateCategoryDto } from '../dto/create-category.dto.js';
+import { CategoryAutocompleteDto } from '../dto/category-autocomplete.dto.js';
+
+@Injectable()
+export class CategoriesService {
+  constructor(
+    private readonly createCategoryProvider: CreateCategoryProvider,
+    private readonly autocompleteCategoryProvider: AutocompleteCategoryProvider,
+    private readonly updateCategoryProvider: UpdateCategoryProvider,
+    private readonly deleteCategoryProvider: DeleteCategoryProvider,
+    private readonly findAllCategoriesProvider: FindAllCategoriesProvider,
+  ) {}
+
+  async findAll(getCategoriesDto: GetCategoriesDto, currentUrl: string) {
+    return await this.findAllCategoriesProvider.findAllCategories(
+      getCategoriesDto,
+      currentUrl,
+    );
+  }
+
+  async createCategory(createCategoryDto: CreateCategoryDto) {
+    return await this.createCategoryProvider.createCategory(createCategoryDto);
+  }
+
+  async getAutocompleteSuggestions(dto: CategoryAutocompleteDto) {
+    return await this.autocompleteCategoryProvider.getSuggestions(dto);
+  }
+
+  async updateCategory(id: number, updateCategoryDto: UpdateCategoryDto) {
+    return await this.updateCategoryProvider.updateCategory(
+      id,
+      updateCategoryDto,
+    );
+  }
+
+  async deleteCategory(id: number) {
+    return await this.deleteCategoryProvider.deleteCategory(id);
+  }
+}
