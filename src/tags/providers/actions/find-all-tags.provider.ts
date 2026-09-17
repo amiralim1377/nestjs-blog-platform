df17c0433b52fb18d8b3b6aa41abeca9596fbf7d
AMIRALI <alimoradi7718@gmail.com>
2026-09-17 12:13:35 +0330

    feat: implement FindAllTagProvider for retrieving tags with pagination and search functionality

---
+++ src/tags/providers/actions/find-all-tags.provider.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Tag } from '../../entities/tag.entity.js';
+import { Repository, ILike } from 'typeorm';
+import { PaginationProvider } from '../../../common/pagination/providers/pagination.providers.js';
+import { GetTagDto } from '../../dto/get-tags.dto.js';
+
+@Injectable()
+export class FindAllTagProvider {
+  constructor(
+    @InjectRepository(Tag)
+    private readonly tagRepository: Repository<Tag>,
+    private readonly paginationProvider: PaginationProvider,
+  ) {}
+
+  public async findAllTag(getTagsDto: GetTagDto, currentUrl: string) {
+    const whereConditions: any = {};
+    if (getTagsDto.search) {
+      whereConditions.name = ILike(`%${getTagsDto.search}%`);
+    }
+
+    return await this.paginationProvider.paginateQuery(
+      getTagsDto,
+      this.tagRepository,
+      currentUrl,
+      {
+        where: whereConditions,
+        order: { createdAt: 'DESC' },
+      },
+    );
+  }
+}
