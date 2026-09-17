504d1f1b806340b4256b2b4ccf1efcdb110c6ef8
AMIRALI <alimoradi7718@gmail.com>
2026-09-17 19:03:31 +0330

    feat: add endpoints to retrieve comments for a specific post and replies for a specific comment

--- src/modules/comments/comments.controller.ts
+++ src/modules/comments/comments.controller.ts
@@ -3,11 +3,11 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
-  Delete,
   HttpCode,
   HttpStatus,
+  Query,
+  Req,
 } from '@nestjs/common';
 import { CommentsService } from './providers/comments.service.js';
 import { CreateCommentDto } from './dto/create-comment.dto.js';
@@ -16,6 +16,8 @@ import { AuthType } from '../auth/enums/auth-type.enum.js';
 import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { ActiveUser } from '../auth/decorator/active-user.decorator.js';
 import type { ActiveUserData } from '../auth/interfaces/active-user-data.interface.js';
+import { GetPostsDto } from '../posts/dto/get-posts.dto.js';
+import type { Request } from 'express';
 
 @Controller('comments')
 export class CommentsController {
@@ -43,5 +45,35 @@ export class CommentsController {
     @ActiveUser() user: ActiveUserData,
   ) {
     return this.commentsService.createComment(createCommentDto, user);
+  }
+
+  @Get('post/:postId')
+  @Auth(AuthType.None)
+  @ApiOperation({
+    summary: 'Get paginated top-level comments for a specific post',
+  })
+  public async getPostComments(
+    @Param('postId') postId: number,
+    @Query() queryDto: GetPostsDto,
+    @Req() request: Request,
+  ) {
+    const currentUrl = `${request.protocol}://${request.get('host')}${request.path}`;
+    return this.commentsService.findPostComments(postId, queryDto, currentUrl);
+  }
+
+  @Get(':commentId/replies')
+  @Auth(AuthType.None)
+  @ApiOperation({ summary: 'Get paginated replies for a specific comment' })
+  public async getCommentReplies(
+    @Param('commentId') commentId: number,
+    @Query() queryDto: GetPostsDto,
+    @Req() request: Request,
+  ) {
+    const currentUrl = `${request.protocol}://${request.get('host')}${request.path}`;
+    return this.commentsService.findCommentReplies(
+      commentId,
+      queryDto,
+      currentUrl,
+    );
   }
 }
