2a9fdbf0364cd565520a5dce5713ccc69fb238d3
AMIRALI <alimoradi7718@gmail.com>
2026-09-17 19:05:11 +0330

    feat: enhance CommentsService with findPostComments and findCommentReplies methods

--- src/modules/comments/providers/comments.service.ts
+++ src/modules/comments/providers/comments.service.ts
@@ -2,15 +2,46 @@ import { Injectable } from '@nestjs/common';
 import { CreateCommentProvider } from './actions/create-comment.provider.js';
 import { CreateCommentDto } from '../dto/create-comment.dto.js';
 import { ActiveUserData } from '../../auth/interfaces/active-user-data.interface.js';
+import { FindPostCommentsProvider } from './actions/find-post-comments.provider.js';
+import { FindCommentRepliesProvider } from './actions/find-comment-replies.provider.js';
+import { GetPostsDto } from '../../posts/dto/get-posts.dto.js';
 
 @Injectable()
 export class CommentsService {
-  constructor(private readonly createCommentProvider: CreateCommentProvider) {}
+  constructor(
+    private readonly createCommentProvider: CreateCommentProvider,
+    private readonly findPostCommentsProvider: FindPostCommentsProvider,
+    private readonly findCommentRepliesProvider: FindCommentRepliesProvider,
+  ) {}
 
   public async createComment(
     createCommentDto: CreateCommentDto,
     user: ActiveUserData,
   ) {
     return await this.createCommentProvider.create(createCommentDto, user);
+  }
+
+  public async findPostComments(
+    postId: number,
+    queryDto: GetPostsDto,
+    currentUrl: string,
+  ) {
+    return await this.findPostCommentsProvider.findByPostId(
+      postId,
+      queryDto,
+      currentUrl,
+    );
+  }
+
+  public async findCommentReplies(
+    commentId: number,
+    queryDto: GetPostsDto,
+    currentUrl: string,
+  ) {
+    return await this.findCommentRepliesProvider.findReplies(
+      commentId,
+      queryDto,
+      currentUrl,
+    );
   }
 }
