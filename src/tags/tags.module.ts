02bcb9538850c33dd4e10645c7da04c3a791cccd
AMIRALI <alimoradi7718@gmail.com>
2026-09-17 12:12:22 +0330

    feat: add FindAllTagProvider and PaginationModule to TagsModule

--- src/tags/tags.module.ts
+++ src/tags/tags.module.ts
@@ -6,15 +6,18 @@ import { UpdateTagProvider } from './providers/actions/update-tag.provider.js';
 import { Tag } from './entities/tag.entity.js';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DeleteTagProvider } from './providers/actions/delete-tag.provider.js';
+import { FindAllTagProvider } from './providers/actions/find-all-tags.provider.js';
+import { PaginationModule } from '../common/pagination/pagination.module.js';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Tag])],
+  imports: [TypeOrmModule.forFeature([Tag]), PaginationModule],
   controllers: [TagsController],
   providers: [
     TagsService,
     CreateTagProvider,
     UpdateTagProvider,
     DeleteTagProvider,
+    FindAllTagProvider,
   ],
 })
 export class TagsModule {}
