af2972faf7d8cbf74f413def0bb3ec4d289d4a83
AMIRALI <alimoradi7718@gmail.com>
2026-09-17 19:04:37 +0330

    feat: add pagination and find providers to CommentsModule for enhanced comment management

--- src/modules/comments/comments.module.ts
+++ src/modules/comments/comments.module.ts
@@ -6,11 +6,24 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Comment } from './entities/comment.entity.js';
 import { CreateCommentProvider } from './providers/actions/create-comment.provider.js';
 import { UsersModule } from '../users/users.module.js';
+import { PaginationProvider } from '../../common/pagination/providers/pagination.providers.js';
+import { FindPostCommentsProvider } from './providers/actions/find-post-comments.provider.js';
+import { FindCommentRepliesProvider } from './providers/actions/find-comment-replies.provider.js';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Comment]), PostsModule, UsersModule],
+  imports: [
+    TypeOrmModule.forFeature([Comment]),
+    PostsModule,
+    UsersModule,
+    PaginationProvider,
+  ],
   controllers: [CommentsController],
-  providers: [CommentsService, CreateCommentProvider],
+  providers: [
+    CommentsService,
+    CreateCommentProvider,
+    FindPostCommentsProvider,
+    FindCommentRepliesProvider,
+  ],
   exports: [CommentsService],
 })
 export class CommentsModule {}
