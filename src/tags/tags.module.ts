99a9aec5bf4fb5d7a80430f8def8425825661b87
AMIRALI <alimoradi7718@gmail.com>
2026-09-17 11:12:26 +0330

    feat: add TypeOrmModule import for Tag entity in TagsModule

--- src/tags/tags.module.ts
+++ src/tags/tags.module.ts
@@ -3,8 +3,11 @@ import { TagsService } from './providers/tags.service.js';
 import { TagsController } from './tags.controller.js';
 import { CreateTagProvider } from './providers/actions/create-tag.provider.js';
 import { UpdateTagProvider } from './providers/actions/update-tag.provider.js';
+import { Tag } from './entities/tag.entity.js';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Tag])],
   controllers: [TagsController],
   providers: [TagsService, CreateTagProvider, UpdateTagProvider],
 })
